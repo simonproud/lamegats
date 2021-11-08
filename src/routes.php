4b76dc25025a60243aac617c4fd124e68e807a1f
simonproud <simon.proud@ro.ru>
2021-11-08 16:00:44 +0300

    fx

--- src/routes.php
+++ src/routes.php
@@ -3,5 +3,5 @@ Route::any(config('vats.entrypoint'), ['as' => 'ats.index',
     'uses' => SimonProud\Lamegats\Controllers\ATSController::class.'@index'
 ]);
 
-Route::post(config('vats.api_prefix').'/calls/make', \SimonProud\Lamegats\Controllers\CallController::class.'@makeCall',  array("as" => "api"));
+Route::post(config('vats.api_prefix').'/calls/{account}/make', \SimonProud\Lamegats\Controllers\CallController::class.'@makeCall',  array("as" => "api"));
 Route::apiResource(config('vats.api_prefix').'/calls', \SimonProud\Lamegats\Controllers\CallController::class,  array("as" => "api"));
