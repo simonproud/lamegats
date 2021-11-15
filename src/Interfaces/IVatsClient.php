ae8d6ef025eb977df6f6851d789c4d607d3dc8f4
Simon Proud <simon.proud@ro.ru>
2021-11-15 11:35:37 +0300

    fx

--- src/Interfaces/IVatsClient.php
+++ src/Interfaces/IVatsClient.php
@@ -5,4 +5,7 @@ namespace SimonProud\Lamegats\Interfaces;
 interface IVatsClient
 {
     public static function getByVats(string $phone, string|null $user):self|null;
+    public function getName():string;
+    public function calls();
+    public function lastCall();
 }
